56617ed2fb1d144d14b985b0b93935d80e8ac022
Temur Programmer <temurnarzullayev810@gmail.com>
2025-09-09 14:56:48 +0300

    added

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -32,17 +32,14 @@ Rasm URL: ${formData.imgUrl}
 `;
 
     try {
-      await fetch(
-        `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            chat_id: CHAT_ID,
-            text: message,
-          }),
-        }
-      );
+      await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          chat_id: CHAT_ID,
+          text: message,
+        }),
+      });
       alert("Ro'yxatdan o'tish muvaffaqiyatli!");
       setFormData({
         username: "",
